e19fbf31e593fe5f3437aeb0763918ca32aac579
Aalxey <infurnus555@gmail.com>
2025-11-11 11:40:05 +0545

    Task1

---
+++ week3/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1
+{
+    internal class First
+    {
+        static int a = 10;
+        static int b = 5;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(First.a+First.b);
+        }
+
+    }
+}
